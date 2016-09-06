d3f307801171403fb82ace1198a0e830f9dfcb08
Prakash Bhatta <pbhatt48@gmail.com>
2016-09-05 23:33:20 -0400

    Making changes

--- Assignment3/src/edu/gatech/seclass/HelloWorld.java
+++ Assignment3/src/edu/gatech/seclass/HelloWorld.java
@@ -5,7 +5,60 @@ package edu.gatech.seclass;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello Workfldfd");
+
+        HelloWorld hello = new HelloWorld();
+
+        hello.letsGetCounters();
+
+        hello.letsGetNthCharacters(3, false);
+
+    }
+
+    public void letsGetCounters() {
+        int counter = 0;
+        String currentString = "I'd b3tt3r put s0me d161ts in this 5tr1n6, right?";
+        for (int i = 0; i < currentString.length(); i++) {
+            char c = currentString.charAt(i);
+            if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9') {
+                counter++;
+                for (int j = i + 1; j < currentString.length(); j++) {
+                    char aCS = currentString.charAt(j);
+                    if (aCS == '0' || aCS == '1' || aCS == '2' || aCS == '3' || aCS == '4' || aCS == '5' || aCS == '6' || aCS == '7' || aCS == '8' || aCS == '9') {
+                        i++;
+                    } else {
+                        break;
+                    }
+
+                }
+                /*char aCS = currentString.charAt(i + 1);
+                if (aCS == '0' || aCS == '1' || aCS == '2' || aCS == '3' || aCS == '4' || aCS == '5' || aCS == '6' || aCS == '7' || aCS == '8' || aCS == '9') {
+                    i++;
+                }*/
+            }
+        }
+        System.out.println("Counter ==  " + counter);
+    }
+
+    public void letsGetNthCharacters(int n, boolean startFromEnd) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("N should be greater than 0");
+        }
+
+        int counter = 0;
+        //String currentString = "I'd b3tt3r put s0me d161ts in this 5tr1n6, right?";
+        String currentString = "apple is a good";
+        if (currentString == null) {
+            throw new NullPointerException("String cannot be null");
+        }
+        String returnString = "";
+        for (int i = n; i < currentString.length(); i = i + n) {
+            char c = currentString.charAt(i);
+            returnString = returnString + c;
+        }
+
+        System.out.println("Return String == " + returnString);
     }
 }
+
+
+
